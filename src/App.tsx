121a70d97df89a793456f4d41c514301571d1ab5
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-02-26 12:05:40 +0800

    testing pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./styles.css";
 
 export const App = () => {
+  const sample = "Testing github pre-commit hook";
+
   return (
     <div>
+      {sample}
       <h1>SalesWhales</h1>
     </div>
   );
